bb8fd35295753fd979b1e2136653939a7f505a96
flaguna99 <60383405+flaguna99@users.noreply.github.com>
2021-05-14 09:41:38 +0200

    Update leerFicheroXML.js

--- 1/leerFicheroXML.js
+++ 1/leerFicheroXML.js
@@ -1,14 +1,6 @@
 function gestionarFicheroXML(xmlDoc){
-	let capaVacia = document.querySelector("#ficheroXML")
-	let alumnos = xmlDoc.querySelectorAll("alumno")
-	for(let i=0; i<alumnos.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
-	
+alert(xmlDoc)
 }
 
-let capa = document.querySelector("div:nth-child(1)") 
-capa.addEventListener("click",CargarFichero);
-function CargarFichero()
-{
-	loadDocA("leerFicheroXML.xml","xml");
-}
+	loadDocA("libros.xml","xml");
+
